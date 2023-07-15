bc25b856a5c589862342f40a0e365fb1fad9eab0
Louie Fitzpatrick <fitz4492@gmail.com>
2023-07-15 13:44:20 -0500

    hero blured circles style edit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,16 @@ const Hero = () => {
     <section class="relative isolate bg-[#121120] min-h-screen px-8 sm:px-20 mb-24">
       <div class="relative pt-14 h-full">
         <div
-          class="absolute rounded-full inset-x-0 -z-10 w-[800px] transform-gpu overflow-hidden blur-3xl left-[-8rem] sm:top-[8rem]"
+          class="absolute rounded-full inset-x-0 -z-10 w-full h-1/2 lg:h-[800px] rotate-45 lg:w-[800px] transform-gpu overflow-hidden blur-3xl opacity-70 left-[2] lg:left-[-8rem] sm:top-[8rem]"
           aria-hidden="true">
           <svg
-            height="800"
-            width="800"
+            height="100%"
+            width="100%"
             class="cloud-gradient blur-3xl opacity-60 overflow-hidden">
             <circle
-              cx="400"
-              cy="400"
-              r="100"
+              cx="50%"
+              cy="50%"
+              r="10%"
               style="fill:transparent;"
             />
           </svg>
@@ -46,16 +46,16 @@ const Hero = () => {
         <WorkLogoList />
       </div>
       <div
-        class="absolute inset-x-0 left-[calc(100%-20rem)] top-[calc(100%-5rem)] w-[400px] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        class="absolute inset-x-0 left-[unset] lg:left-[calc(100%-400px)] right-4 lg:right-[unset] top-[calc(100%-5rem)] w-1/2 lg:w-[400px] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
         aria-hidden="true">
         <svg
-          height="400"
-          width="400"
-          class="relative rounded-full bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20">
+          height="100%"
+          width="100%"
+          class="relative rounded-full cloud-gradient opacity-20">
           <circle
-            cx="200"
-            cy="200"
-            r="90"
+            cx="50"
+            cy="50"
+            r="20%"
             style="fill:white;stroke:purple;stroke-width:2"
           />
         </svg>
